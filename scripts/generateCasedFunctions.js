105df494b73e6424a8681afb908d1c74f8ce3935
Alex J Burke <alex@alexjeffburke.com>
2019-12-07 13:12:58 +0100

    Generate the set of available assertions in base export at build time.
    An issue with the previous version of this code was well as many of
    previous "façade" implementations is that generating the conversion
    glue resulted in highly dynamic code that made it rather difficult
    to get a sense of what was implemented and how it was done.
    
    Attempt to address this by ensuring a hard-coded list of supported
    assertions is shipped as part of the built artefacts.

---
+++ scripts/generateCasedFunctions.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const prettier = require("prettier");
+
+const createCasedFunctions = require("../lib/createCasedFunctions");
+const prepareAssertions = require("../lib/prepareAssertions");
+
+function prepareFunctions(expect) {
+  const assertions = prepareAssertions(expect);
+  const { casedFunctions, casedMap } = createCasedFunctions(expect, assertions);
+
+  const functions = Object.keys(casedFunctions).map(key => {
+    const { assertionString } = casedMap[key];
+    let fnString = casedFunctions[key].toString();
+    // eslint-disable-next-line no-template-curly-in-string
+    fnString = fnString.replace("${__camelCasedString__}", key);
+    // eslint-disable-next-line no-template-curly-in-string
+    fnString = fnString.replace("__assertionString__", `"${assertionString}"`);
+    return `    ${key}: ${fnString}`;
+  });
+
+  return `module.exports = expect => ({\n${functions.join(",\n")}\n})\n`;
+}
+
+function generateCasedFunctions(outputFile) {
+  const expect = require("unexpected");
+  const output = formatSource(prepareFunctions(expect));
+
+  fs.writeFileSync(path.resolve(process.cwd(), outputFile), output, "utf8");
+}
+
+function formatSource(code) {
+  const prettierOptions = prettier.resolveConfig.sync(__filename, {
+    editorconfig: true
+  });
+
+  return prettier.format(code, { ...prettierOptions, parser: "babel" });
+}
+
+module.exports = generateCasedFunctions;
+
+if (require.main === module) {
+  generateCasedFunctions(process.argv[2]);
+}
