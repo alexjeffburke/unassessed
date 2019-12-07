64b4538843551769cc2656f703c91af9f26c76d4
Alex J Burke <alex@alexjeffburke.com>
2019-12-07 14:05:45 +0100

    Dedupe value type determination in .d.ts generator after 85b473e.

--- scripts/generateTypescriptDefinition.js
+++ scripts/generateTypescriptDefinition.js
@@ -6,13 +6,6 @@ const path = require("path");
 const createCasedFunctions = require("../lib/createCasedFunctions");
 const prepareAssertions = require("../lib/prepareAssertions");
 
-const ASSERTIONS_TO_FORCE_NO_VALUE = {
-  "to be falsy": true,
-  "to be truthy": true,
-  "not to be falsy": true,
-  "not to be truthy": true
-};
-
 const TYPE_NAMES_TO_EXCLUDE = {
   assertion: true,
   type: true,
@@ -48,34 +41,8 @@ function populateTempalate(expect) {
   const { casedFunctions, casedMap } = createCasedFunctions(expect, assertions);
 
   const matchers = Object.keys(casedFunctions).map(key => {
-    const assertionString = casedMap[key];
-    let originalAssertion = expect.assertions[assertionString];
+    const { typesOfValues } = casedMap[key];
 
-    // TODO: remove this workaround when "to be ok" string value it removed
-    if (assertionString in ASSERTIONS_TO_FORCE_NO_VALUE) {
-      originalAssertion = [];
-    }
-
-    const typesOfValues = [];
-    originalAssertion.forEach(definition => {
-      const valueMatches = definition.declaration.match(
-        /(?: <([a-zA-z-]+[?]?(?:[|][a-zA-z-]+)*)>)* <([a-zA-z-]+[?]?(?:[|][a-zA-z-]+)*)>$/
-      );
-      if (valueMatches) {
-        const validMatches = valueMatches.slice(1).filter(Boolean);
-        validMatches.forEach((valueMatch, index) => {
-          if (!valueMatch) return;
-          let typeOfValue;
-          if (typesOfValues.length <= index) {
-            typeOfValue = new Set();
-            typesOfValues.push(typeOfValue);
-          } else {
-            typeOfValue = typesOfValues[index];
-          }
-          valueMatch.split("|").map(type => typeOfValue.add(type));
-        });
-      }
-    });
     if (typesOfValues.length === 0) {
       return `${key}(): Result`;
     }
