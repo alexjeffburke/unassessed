5e14cdb6545f7f2b1ba3b08af93218c2851f219c
Alex J Burke <alex@alexjeffburke.com>
2019-12-12 18:51:03 +0000

    Add a test file to check the registration of U-dom assertion.

---
+++ test/plugins/dom.js
@@ -0,0 +1,53 @@
+const expect = require("unexpected")
+  .clone()
+  .use(require("unexpected-dom"));
+
+const assess = require("../../lib/unassessed");
+
+describe("plugins/dom", () => {
+  let assessWithDom;
+
+  beforeEach(() => {
+    assessWithDom = assess.fromInstance(expect);
+  });
+
+  it("should expose assertions defined by the plugin", () => {
+    const partialExpect = assessWithDom(undefined);
+
+    // complete assertion to avoid afterEach() hook
+    partialExpect.toBeUndefined();
+
+    const keys = Object.keys(partialExpect);
+    const assessWithDomKeys = keys.slice(
+      // every index after the last core assertion
+      keys.indexOf("toCallTheCallbackWithError") + 1
+    );
+
+    expect(assessWithDomKeys, "to equal", [
+      "toHaveClass",
+      "toHaveClasses",
+      "toOnlyHaveClass",
+      "toOnlyHaveClasses",
+      "notToHaveClass",
+      "notToHaveClasses",
+      "toOnlyHaveAttribute",
+      "toOnlyHaveAttributes",
+      "toHaveAttribute",
+      "toHaveAttributes",
+      "notToHaveAttribute",
+      "notToHaveAttributes",
+      "toHaveNoChild",
+      "toHaveNoChildren",
+      "toHaveChild",
+      "toHaveChildren",
+      "toHaveText",
+      "toContainNoElementsMatching",
+      "toContainElementsMatching",
+      "notToContainElementsMatching",
+      "notToContainTestId",
+      "toContainTestId",
+      "notToHaveTestId",
+      "toHaveTestId"
+    ]);
+  });
+});
