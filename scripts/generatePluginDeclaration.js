d502e57673b5d15fc122112a3595204e4ce0ad53
Alex J Burke <alex@alexjeffburke.com>
2019-12-18 14:35:18 +0100

    Implement value type diffing to ensure extended assertion inclusion.
    One common thing assertions do is to add additional types and then
    define some of the standard comparisons for them. The previous code
    naiively discarded any assertions whose name was previously seen.
    Make this a diff on the types of the values to handle this properly.

--- scripts/generatePluginDeclaration.js
+++ scripts/generatePluginDeclaration.js
@@ -1,10 +1,19 @@
 const expect = require("unexpected");
 const prepareAssertions = require("../src/prepareAssertions");
 const processUnexpectedInstance = require("../src/processUnexpectedInstance");
+const { identifyTypesOfValues } = processUnexpectedInstance;
 const {
   populateTempalate,
   readTemplate
 } = require("./generateTypescriptDeclaration");
+
+function withoutSet(set1, set2) {
+  const s = new Set();
+  for (const item of set1) {
+    if (!set2.has(item)) s.add(item);
+  }
+  return s;
+}
 
 function generatePluginDeclaration(plugin) {
   const assertions = prepareAssertions(expect);
@@ -19,7 +28,26 @@ function generatePluginDeclaration(plugin) {
 
   const pluginCasedDefinitions = {};
   for (const [fnName, def] of Object.entries(casedDefinitionsWithPlugin)) {
-    if (casedDefinitions[fnName]) continue;
+    if (casedDefinitions[fnName]) {
+      const typesOfValues = [];
+      const typesOfValuesLeft = casedDefinitions[fnName].typesOfValues;
+      const typesOfValuesRight = def.typesOfValues;
+
+      for (const [index, typeSetRight] of typesOfValuesRight.entries()) {
+        const typeSetLeft = typesOfValuesLeft[index] || new Set();
+
+        typesOfValues.push(withoutSet(typeSetRight, typeSetLeft));
+      }
+
+      if (typesOfValues.every(s => s.size === 0)) {
+        continue;
+      } else {
+        Object.assign(def, {
+          ...identifyTypesOfValues(typesOfValues),
+          typesOfValues
+        });
+      }
+    }
     if (def.isMiddleRocket) continue;
     pluginCasedDefinitions[fnName] = def;
   }
