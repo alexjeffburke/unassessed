07ef1c727afc325dd35aa5a3c34c016cb56b36c5
Alex J Burke <alex@alexjeffburke.com>
2019-12-20 10:33:50 +0100

    Split the spawnInDir() test functions out as a util.

--- test/integration/mocha.js
+++ test/integration/mocha.js
@@ -1,60 +1,7 @@
 const expect = require("unexpected");
 const path = require("path");
-const spawn = require("child_process").spawn;
 
-function spawnInDir(cwd, bin, args) {
-  const spawnedCli = spawn(bin, args, { cwd });
-
-  const p = new Promise((resolve, reject) => {
-    let sawExit = false;
-    let stdout = "";
-    let stderr = "";
-
-    spawnedCli.stdout.on("data", chunk => {
-      stdout += chunk.toString("utf8");
-    });
-
-    spawnedCli.stderr.on("data", chunk => {
-      stderr += chunk.toString("utf8");
-    });
-
-    const makeError = code => {
-      const error = new Error("spawnCli error");
-      error.code = code;
-      error.stdout = stdout;
-      error.stderr = stderr;
-      return error;
-    };
-
-    spawnedCli.on("error", () => {
-      if (sawExit) {
-        return;
-      }
-
-      sawExit = true;
-
-      reject(makeError(null));
-    });
-
-    spawnedCli.on("exit", code => {
-      if (sawExit) {
-        return;
-      }
-
-      sawExit = true;
-
-      if (code) {
-        reject(makeError(code));
-      } else {
-        resolve({ stdout, stderr });
-      }
-    });
-  });
-
-  p._spawn = spawnedCli;
-
-  return p;
-}
+const spawnInDir = require("../utils/spawnInDir");
 
 const MOCHA_BIN = path.join(
   __dirname,
